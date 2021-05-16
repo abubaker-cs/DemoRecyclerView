82bc2836f0678d63b7b8dc13261faf0c69b66900
abubaker <mohammad_abubaker@yahoo.com>
2021-05-16 12:26:33 +0500

    Base setup

--- app/src/main/java/org/abubaker/demorecyclerview/MainActivity.kt
+++ app/src/main/java/org/abubaker/demorecyclerview/MainActivity.kt
@@ -2,13 +2,19 @@ package org.abubaker.demorecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.demorecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+
     }
 
 
